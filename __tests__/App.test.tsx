746a3725438519952ac09a7e48e1b3438bad635f
Vladislav <prorok210c@gmail.com>
2025-10-01 01:44:06 +0300

    feat: добавлены тесты (#10)

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -2,9 +2,19 @@ import React from 'react';
 import { render } from '@testing-library/react-native';
 import App from '../App';
 
+// Мокаем expo-status-bar
+jest.mock('expo-status-bar', () => ({
+  StatusBar: 'StatusBar',
+}));
+
 describe('App', () => {
   it('renders without crashing', () => {
+    const component = render(<App />);
+    expect(component).toBeTruthy();
+  });
+
+  it('renders auth screen by default', () => {
     const { getByText } = render(<App />);
-    expect(getByText('Анализатор мусора')).toBeTruthy();
+    expect(getByText('Войти в аккаунт')).toBeTruthy();
   });
 });
